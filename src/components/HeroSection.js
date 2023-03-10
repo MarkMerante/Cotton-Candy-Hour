50cdb40dbf1489938522cdcbb4d3d659f1e0c2b7
MarkMerante <markmerante9@gmail.com>
2023-03-10 10:55:14 -0500

    Stops reloading the Hero Section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,9 +5,33 @@ import HomeHero from '../images/home-hero2.PNG'
 import HomeHeroVid from '../images/home-hero.MP4'
 
 function HeroSection() {
+    const videoRef = useRef(null);
+    const [isMobile, setIsMobile] = useState(false)
+
+    useEffect(() => {
+        function handleResize() {
+        setIsMobile(window.innerWidth <= 999)
+    }
+
+        window.addEventListener('resize', handleResize)
+        handleResize()
+
+        return () => {
+          window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
+    useEffect(() => {
+        if (isMobile) {
+            videoRef.current.pause()
+        } else {
+            videoRef.current.play()
+        }
+    }, [isMobile])
+
     return (
         <div className='hero-container'>
-            <video autoPlay muted={true} loop={true} src={HomeHeroVid} />
+            <video autoPlay muted={true} loop={true} src={HomeHeroVid} ref={videoRef}/>
             <img src={HomeHero} className='ChiHome' alt="Hero Image" />
         </div>
     )
